ad3041366673e4d935cb7ec64c3f3e0cf4dcc225
SWASTIK CHATTERJEE <chatterjeeswastik7772@gmail.com>
2024-01-31 19:42:12 +0530

    second commit

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,12 @@
 const express=require("express");
 const rootRouter=require("./routes/index");
+const cors=require("cors")
 
+app.use(cors())
+app.use(express.json());
 const app=express();
 
-app.use(/api/v1, rootRouter);
+
+app.use("/api/v1", rootRouter);
+
+app.listen(3000);
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -4,6 +4,7 @@ const userRouter=require("./user")
 
 const router=express.Router();
 
-router.use("/user", userRouter);
+router.use("/user", userRouter); // whichever request is made to /user is redirected to the userRouter.
+// this userRouter is exported from the ./user.js file. we import it from there and here naming it as the userRouter.
 
 module.exports=router;
